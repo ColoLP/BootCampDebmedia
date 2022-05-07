b36ec9915af4c044f4d8688136f9bddd370c06d9
agusgomez04 <105002047+agusgomez04@users.noreply.github.com>
2022-05-06 21:01:24 -0300

    First commit

---
+++ src/PruebaGitAgus/Matrices.java
@@ -0,0 +1,10 @@
+public class Matrices {
+    public static void main(String[] args) {
+        int[][] numeros = new int[2][4];
+
+        numeros[0][0] = 1;
+        numeros[0][1] = 2;
+        numeros[0][2] = 3;
+        numeros[0][3] = 4;
+    }
+}
